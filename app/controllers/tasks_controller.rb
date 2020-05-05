5597f274b8d562dcb38c0367c547774c80b09c03
user.nanasi <zhuanyongxuexi22@gmail.com>
2020-05-05 04:48:12 +0000

    閲覧制限の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :user_logged_in
+  before_action :user_logged_in, only: [:index, :show]
+   before_action :correct_user, only: [:destroy]
   
   def index
     if logged_in?
