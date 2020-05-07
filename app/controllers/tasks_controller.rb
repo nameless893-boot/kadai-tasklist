9ecba198fbf7c1eea7ea864f214ddc24005355ba
user.nanasi <zhuanyongxuexi22@gmail.com>
2020-05-07 09:05:47 +0000

    correct_userからupdateを消去

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :user_logged_in
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -30,7 +30,6 @@ class TasksController < ApplicationController
   end
   
   def update
-
     if @task.update(task_params)
       flash[:success] = 'Taskが更新されました'
       redirect_to @task
